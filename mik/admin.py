4348313556dbfdcbcc34a7e510b83a55e7d67be4
nic <nic@nics-MacBook-Pro.local>
2020-02-25 20:11:51 +0330

    first model

--- mik/admin.py
+++ mik/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import IpAddress
 
-# Register your models here.
+
+admin.site.register(IpAddress)
--- mik/models.py
+++ mik/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class IpAddress(models.Model):
+    pub_date = models.DateTimeField('date published')
+    ip_address = models.GenericIPAddressField()
+    port = models.IntegerField()
