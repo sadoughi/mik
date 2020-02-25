8fb6a30ae2d318c972bcd49f9a853f6c4cffb6fc
nic <nic@nics-MacBook-Pro.local>
2020-02-25 21:45:13 +0330

    show ip and port

--- mik/models.py
+++ mik/models.py
@@ -4,3 +4,5 @@ class IpAddress(models.Model):
     pub_date = models.DateTimeField('date published')
     ip_address = models.GenericIPAddressField()
     port = models.IntegerField()
+    def __str__(self):
+        return "{}:{}".format(self.ip_address,self.port)
